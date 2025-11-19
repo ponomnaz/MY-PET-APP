22735b8fd9c8c6caf5e5dd7baf663c085813cd3f
ponomnaz <ponomarev.naz@gmail.com>
2025-11-19 12:34:14 +0300

    Test

--- backend/app/main.py
+++ backend/app/main.py
@@ -3,11 +3,25 @@ from enum import Enum
 from fastapi import FastAPI
 from pydantic import BaseModel
 
+
 class Item(BaseModel):
     name: str
     description: str | None = None
     price: float
     tax: float | None = None
+
+    model_config = {
+        "json_schema_extra": {
+            "examples": [
+                {
+                    "name": "Foo",
+                    "description": "A very nice Item",
+                    "price": 35.4,
+                    "tax": 3.2,
+                }
+            ]
+        }
+    }
 
 class ModelName(str, Enum):
     alexnet = "alexnet"
